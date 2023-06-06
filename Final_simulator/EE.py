4f56e9b1ff3ad0956bbd1662cd9382d97e03c948
somay-jalan <118368538+somay-jalan@users.noreply.github.com>
2023-06-06 21:30:32 +0530

    Adding execution funtion in EE.py

--- Final_simulator/EE.py
+++ Final_simulator/EE.py
@@ -210,3 +210,54 @@ def je(mem_addr):
         flags_value="0"*16
         Reg_value[instruct.registers["FLAGS"]]=flags_value
         return -1
+
+
+def execute(instruction):
+    if instruction[0:5]=="00000":
+        pc=add(instruction[7:10],instruction[10:13],instruction[13:16])
+    elif instruction[0:5]=="00001":
+        pc=sub(instruction[7:10],instruction[10:13],instruction[13:16])
+    elif instruction[0:5]=="00110":
+        pc=mul(instruction[7:10],instruction[10:13],instruction[13:16])
+    elif instruction[0:5]=="01010":
+        pc=xor(instruction[7:10],instruction[10:13],instruction[13:16])
+    elif instruction[0:5]=="01011":
+        pc=Or(instruction[7:10],instruction[10:13],instruction[13:16])
+    elif instruction[0:5]=="01100":
+        pc=And(instruction[7:10],instruction[10:13],instruction[13:16])
+    elif instruction[0:5]=="00010":
+        pc=movImm(instruction[6:9],instruction[9:16])
+    elif instruction[0:5]=="01000":
+        pc=rs(instruction[6:9],instruction[9:16])
+    elif instruction[0:5]=="01001":
+        pc=ls(instruction[6:9],instruction[9:16])
+    elif instruction[0:5]=="00011":
+        pc=movreg(instruction[10:13],instruction[13:16])
+    elif instruction[0:5]=="01101":
+        pc=invert(instruction[10:13],instruction[13:16])
+    elif instruction[0:5]=="01110":
+        pc=compare(instruction[10:13],instruction[13:16])
+    elif instruction[0:5]=="00100":
+        pc=load(instruction[6:9],instruction[9:16])
+    elif instruction[0:5]=="00101":
+        pc=store(instruction[6:9],instruction[9:16])
+    elif instruction[0:5]=="01111":
+        pc=jmp(instruction[9:16])
+    elif instruction[0:5]=="11100":
+        pc=jlt(instruction[9:16])
+    elif instruction[0:5]=="11101":
+        pc=jgt(instruction[9:16])
+    elif instruction[0:5]=="11111":
+        pc=je(instruction[9:16])
+    elif instruction[0:5]=="11010":
+        json_dict=dict()
+        json_dict["binary"]=binary
+        json_dict={"binary":binary,"error":[]}
+        with open("memory.json","w") as memory:
+            json.dump(json_dict,memory)
+        return "Halt"
+    if pc==-1:
+        return "normal"
+    else:
+        return pc
+    
